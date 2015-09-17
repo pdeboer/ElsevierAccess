cdd71228c32c7f168c4187cc9c94feb176b21e03
Mattia Amato <mattia.amato@uzh.ch>
2015-09-17 11:08:36 +0200

    Init commit

---
+++ src/main/scala-2.11/ch/uzh/ifi/pdeboer/elsevieraccess/ElsevierApp.scala
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.pdeboer.elsevieraccess
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+ * Created by mattia on 17.09.15.
+ */
+object ElsevierApp extends App with LazyLogging{
+
+  logger.debug("App")
+
+}
